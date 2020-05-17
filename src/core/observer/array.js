2d1bc01156e3b86229cf9aa5becb1495d06d25d0
kscript <1258@live.cn>
2020-05-17 15:34:37 +0800

    分析vue对数组方法的处理

--- src/core/observer/array.js
+++ src/core/observer/array.js
@@ -17,7 +17,10 @@ const methodsToPatch = [
   'sort',
   'reverse'
 ]
-
+// >10 
+// 对数组方法的处理
+// push,unshift,splice都有可能涉及到新的元素, 需要对这些元素添加关注
+// 其它的, 都只将调用原生结果返回, 并通知订阅列表更新
 /**
  * Intercept mutating methods and emit events
  */
